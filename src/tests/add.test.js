5dbd1b96ea4b3157538d10ecea321070325e8742
dwikyerl <dwikyerl@gmail.com>
2018-02-10 15:13:37 +0700

    Add example test cases for jest

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,17 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}`;
 
 test('should add two numbers', () => {
   const res = add(3,4);
   expect(res).toBe(7);
+});
+
+test('should return Hello with given name', () => {
+  const res = generateGreeting('Joko');
+  expect(res).toBe('Hello Joko');
+});
+
+test('should return Hello with no name', () => {
+  const res = generateGreeting();
+  expect(res).toBe('Hello Anonymous');
 });
